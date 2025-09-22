5618545b521440808555e32b54204aff320ad3e8
Westley Holmes <westleyholmes@Mac.attlocal.net>
2025-09-22 16:10:16 -0700

    fix: Add comment to trigger Azure redeployment and fix indentation issue

--- app.py
+++ app.py
@@ -3369,5 +3369,6 @@ def create_tables():
 if __name__ == '__main__':
     create_tables()
     # Use standard Flask run - SocketIO removed
+    # Fixed indentation issue for Azure deployment
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
